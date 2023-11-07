4d22e32401787c6ec5a26822497f7d28377cbd56
Jcruz0420 <149451145+Jcruz0420@users.noreply.github.com>
2023-11-06 20:12:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     """
     celcius = int(input())
     farenheit = (celcius * 9/5) + 32
-    print(f'{farenheit:.2f}'
+    print(f'{farenheit:.2f}')
     """
     ########################################
     # Do not delete the return statement
     ########################################
     """
-    return celsius, fahrenheit
+    return celcius, fahrenheit
 
 
 if __name__ == '__main__':
