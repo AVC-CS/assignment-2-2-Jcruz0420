452b3e23a8c4d48463ff2820c72ae411abb7c687
Jcruz0420 <149451145+Jcruz0420@users.noreply.github.com>
2023-11-06 20:14:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     """
     celcius = int(input())
     fahrenheit = (celcius * 9/5) + 32
-    print(f'{farenheit:.2f}')
+    print(f'{fahrenheit:.2f}')
     """
     ########################################
     # Do not delete the return statement
